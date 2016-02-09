74040eda7e5cac5163969711f1cba7f77d6b1133
Joe Feltis <jfeltis@gmail.com>
2016-02-09 15:58:15 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 var express = require('express'),
-		app = express(),
-		util = require('./js/util');
+    app = express(),
+    util = require('./js/util');
 
 app.set('port', process.env.PORT || 1337);
 
 app.post('/users', function(req, res){
-	var users = [],
-			recordsToReturn = (!req.query.r ? 10 : (req.query.r < 1000 ? req.query.r : 1000));
+  var users = [],
+      recordsToReturn = (!req.query.r ? 10 : (req.query.r < 1000 ? req.query.r : 1000));
 
-	for(var i = 0; i < recordsToReturn; i++){
-		var fn = util.getFirstName(),
-				ln = util.getLastName(),
-				e = util.getEmail(fn, ln),
-				p = util.getPhone(i);
+  for(var i = 0; i < recordsToReturn; i++){
+    var fn = util.getFirstName(),
+        ln = util.getLastName(),
+        e = util.getEmail(fn, ln),
+        p = util.getPhone(i);
 
-		users.push({
-			id: i,
-			first_name: fn,
-			last_name: ln,
-			email: e,
-			phone: p
-		});
-	}
+    users.push({
+      id: i,
+      first_name: fn,
+      last_name: ln,
+      email: e,
+      phone: p
+    });
+  }
 
-	res.setHeader('content-type', 'application/json');
-	res.end(JSON.stringify(users));
+  res.setHeader('content-type', 'application/json');
+  res.end(JSON.stringify(users));
 });
 
 app.listen(app.get('port'), function(){
-	console.log(`listning on ${app.get('port')}`);
+  console.log(`listning on ${app.get('port')}`);
 });
--- js/util.js
+++ js/util.js
@@ -1,19 +1,19 @@
 exports.getFirstName = function(){
-	var names = ['James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard', 'Charles', 'Joseph', 'Thomas', 'Christopher', 'Daniel', 'Paul', 'Mark', 'Donald', 'George'];
+  var names = ['James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard', 'Charles', 'Joseph', 'Thomas', 'Christopher', 'Daniel', 'Paul', 'Mark', 'Donald', 'George'];
 
-	return names[Math.floor(Math.random() * names.length)];
+  return names[Math.floor(Math.random() * names.length)];
 };
 
 exports.getLastName = function(){
-	var names = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Anderson', 'Taylor', 'Wilson', 'Thomas', 'Moore', 'Martin', 'Jackson', 'Thompson', 'White'];
+  var names = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Anderson', 'Taylor', 'Wilson', 'Thomas', 'Moore', 'Martin', 'Jackson', 'Thompson', 'White'];
 
-	return names[Math.floor(Math.random() * names.length)];
+  return names[Math.floor(Math.random() * names.length)];
 };
 
 exports.getEmail = function(fn, ln){
-	return `${fn.substring(0, 1).toLowerCase()}${ln.toLowerCase()}@email.com`;
+  return `${fn.substring(0, 1).toLowerCase()}${ln.toLowerCase()}@email.com`;
 };
 
 exports.getPhone = function(i){
-	return '555.555.5555'.substr(0, (12 - String(i).length)) + i;
+  return '555.555.5555'.substr(0, (12 - String(i).length)) + i;
 };
